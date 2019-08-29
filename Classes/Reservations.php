30c4b17dc9b4d30970d823ba28690055bceaef70
Ahnna <ahnnaostling@gmail.com>
2019-08-29 16:24:49 +0200

    add a new fetch all reservations function for use in admin

--- Classes/Reservations.php
+++ Classes/Reservations.php
@@ -4,22 +4,15 @@ include './database-connection.php';
 
 class Reservations {
 
-    // private $pdo;
-
     public $res_id;
-    public $res_guest;
+    public $res_guests;
     public $res_date;
     public $res_time;
     public $res_name;
     public $res_email;
     public $res_tel; 
 
-    // public function __construct($pdo)
-    // {
-        // $this->pdo = $pdo;
-    // }
-
-    public function fetchReservations($pdo) {
+    public function fetchReservationsByDate($pdo) {
         $statement = $pdo->prepare("SELECT * FROM `reservations` 
             WHERE res_date = :res_date "); 
             $statement->execute(
@@ -42,5 +35,23 @@ class Reservations {
             }
 
             return json_encode($listWithRes);
+    }
+
+    public function adminReservationFetch($pdo) {
+        $statement = $pdo->prepare("SELECT * FROM reservations");
+            $statement->execute();
+            $allRes = [];
+            while($row = $statement->fetch(PDO::FETCH_OBJ)) {
+                array_push($allRes, [
+                    "id" => $row->res_id,
+                    "guests" => $row->res_guests,
+                    "date" => $row->res_date,
+                    "time" => $row->res_time,
+                    "name" => $row->res_name,
+                    "email" => $row->res_email,
+                    "phone" => $row->res_tel,
+                ]);
+            }
+            return json_encode($allRes);
     }
 }
--- reservations.php
+++ reservations.php
@@ -1,11 +1,18 @@
 <?php
 session_start();
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 
 include './Classes/Reservations.php';
 
-$reservation = new Reservations();
-$test1 = $reservation->fetchReservations($pdo);
-echo json_encode($test1);
 
+
+$reservation = new Reservations();
+$test1 = $reservation->fetchReservationsByDate($pdo);
+echo ($test1);
+
+$allRes = new Reservations();
+$adminRes = $allRes->adminReservationFetch($pdo);
+echo ($adminRes);
 ?>
 
