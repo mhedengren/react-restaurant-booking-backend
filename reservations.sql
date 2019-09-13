3217d01576df8187bc92ef9a9544a9573e9f4b7a
mhedengren <melker.hedengren@medieinstitutet.se>
2019-09-13 14:39:01 +0200

    update db file

--- reservations.sql
+++ reservations.sql
@@ -1,54 +1,74 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Aug 28, 2019 at 09:12 AM
--- Server version: 5.6.34-log
--- PHP Version: 7.2.1
+-- Värd: localhost:8889
+-- Tid vid skapande: 13 sep 2019 kl 12:37
+-- Serverversion: 5.7.23
+-- PHP-version: 7.2.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `Restaurant`
+-- Databas: `Restaurant`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reservations`
+-- Tabellstruktur `reservations`
 --
 
 CREATE TABLE `reservations` (
   `res_id` int(11) NOT NULL,
   `res_guests` int(11) NOT NULL,
-  `res_date` date NOT NULL,
+  `res_date` varchar(50) NOT NULL,
   `res_time` int(11) NOT NULL,
-  `res_name` int(11) NOT NULL,
+  `res_name` varchar(500) NOT NULL,
   `res_email` varchar(10000) NOT NULL,
-  `res_tel` int(11) NOT NULL
+  `res_tel` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Indexes for dumped tables
+-- Dumpning av Data i tabell `reservations`
+--
+
+INSERT INTO `reservations` (`res_id`, `res_guests`, `res_date`, `res_time`, `res_name`, `res_email`, `res_tel`) VALUES
+(188, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(189, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(190, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(191, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(192, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(193, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(194, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(195, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(196, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(197, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(198, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(199, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(200, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(201, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(202, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(203, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050'),
+(204, 4, '2019-09-20', 18, 'Frida', 'frida@person.se', '070-5055050');
+
+--
+-- Index för dumpade tabeller
 --
 
 --
--- Indexes for table `reservations`
+-- Index för tabell `reservations`
 --
 ALTER TABLE `reservations`
   ADD PRIMARY KEY (`res_id`);
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- AUTO_INCREMENT för dumpade tabeller
+--
+
+--
+-- AUTO_INCREMENT för tabell `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `res_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=205;
